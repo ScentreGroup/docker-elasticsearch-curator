376d46a715a4fed236fa9b5d84fde57cb6d8f884
Jay Ding <jding@scentregroup.com>
2018-04-05 13:07:32 +1000

    Update bash command line string setting

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-bash -c "while true; do curator_cli --host $ELASTICSEARCH_HOST $ELASTICSEARCH_CURATOR_ARGS delete_indices --filter_list "'\[\{\"filtertype\":\"age\",\"source\":\"creation_date\",\"direction\":\"older\",\"unit\":\"days\",\"unit_count\":\"${OLDER_THAN_IN_DAYS}\"\}\]'"; set -e; sleep $(( 60*60*INTERVAL_IN_HOURS )); set +e; done"
+bash -c "while true; do curator_cli --host $ELASTICSEARCH_HOST $ELASTICSEARCH_CURATOR_ARGS delete_indices --filter_list "[{\"filtertype\":\"age\",\"source\":\"creation_date\",\"direction\":\"older\",\"unit\":\"days\",\"unit_count\":\"${OLDER_THAN_IN_DAYS}\"\}\]"; set -e; sleep $(( 60*60*INTERVAL_IN_HOURS )); set +e; done"
 
 # As argument is not related to curator,
 # then assume that user wants to run his own process,
